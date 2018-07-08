cd37deea9e99f3b77144b60908f213e65172a3c8
seckawijoki@gmail.com <seckawijoki@gmail.com>
2018-07-08 22:07:31 +0800

    2018年07月 8日 22:07:30

--- __call_git_add_commit_push.bat
+++ __call_git_add_commit_push.bat
@@ -1,11 +1,16 @@
 @echo off
 
+set fileName=__common_git_add_commit_push.sh
+copy %fileName% f:\cpp_files\%fileName%
+copy %fileName% f:\lua_files\%fileName%
+copy %fileName% f:\working_notes_of_Miniworld\%fileName%
+
 echo Running git pushes of batch files...
-call f:\batch_files\__common_git_add_commit_push.sh
+call f:\batch_files\%fileName%
 echo Running git pushes of cpp files...
-call f:\cpp_files\__common_git_add_commit_push.sh
+call f:\cpp_files\%fileName%
 echo Running git pushes of lua files...
-call f:\lua_files\__common_git_add_commit_push.sh
+call f:\lua_files\%fileName%
 echo Running git pushes of working note files...
-call f:\working_notes_of_Miniworld\__common_git_add_commit_push.sh
+call f:\working_notes_of_Miniworld\%fileName%
 echo Finish all git pushes.
--- __call_git_pull.bat
+++ __call_git_pull.bat
@@ -1,11 +1,16 @@
 @echo off
 
+set fileName=__common_git_pull.sh
+copy %fileName% f:\cpp_files\%fileName%
+copy %fileName% f:\lua_files\%fileName%
+copy %fileName% f:\working_notes_of_Miniworld\%fileName%
+
 echo Running git pulls of batch files...
-call f:\batch_files\__common_git_pull.sh
+call f:\batch_files\%fileName%
 echo Running git pulls of cpp files...
-call f:\cpp_files\__common_git_pull.sh
+call f:\cpp_files\%fileName%
 echo Running git pulls of lua files...
-call f:\lua_files\__common_git_pull.sh
+call f:\lua_files\%fileName%
 echo Running git pulls of working note files...
-call f:\working_notes_of_Miniworld\__common_git_pull.sh
+call f:\working_notes_of_Miniworld\%fileName%
 echo Finish all git pulls.
