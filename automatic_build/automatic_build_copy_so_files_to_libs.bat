1538194549359557795bce648547e8e044544912
seckawijoki@gmail.com <seckawijoki@gmail.com>
2019-08-10 11:51:00 +0800

    2019/08/10 周六 11:50:59.50

--- automatic_build/automatic_build_copy_so_files_to_libs.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ---------- ndk-build end -----------
-set mEndTimeNdkBuild=%time%
-
-if not exist %mPathCurrentProjAndroid%\libs\%mNdkBuildAbi% mkdir %mPathCurrentProjAndroid%\libs\%mNdkBuildAbi%
-
-echo ----- copy so files to %mDirCurrentProjAndroid%\libs start -----
-copy %mPathProjAndroidNdkBuild%\%DIR_NDK_BUILD_OUT%\%mNdkBuildAbi%\* %mPathCurrentProjAndroid%\libs\%mNdkBuildAbi%
-echo ----- copy so files to %mDirCurrentProjAndroid%\libs end -----
--- automatic_build/prebuild_tortoise_svn_update.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-%DRIVE%
-cd %DRIVE%\%DIR_TRUNK%\%DIR_ENV_1%  
-svn update
